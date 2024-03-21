4c2b0029fe32e0ebec2bf77a946a187f91463182
Gaurav <gauravgokhale5@gmail.com>
2024-03-22 02:56:43 +0530

    chat screen scrollview added

--- EduConsult/lib/presentation/chat_screen/chat_screen.dart
+++ EduConsult/lib/presentation/chat_screen/chat_screen.dart
@@ -6,6 +6,7 @@ import 'package:educonsult/widgets/app_bar/appbar_title_circleimage.dart';
 import 'package:educonsult/widgets/app_bar/appbar_subtitle_one.dart';
 import 'package:educonsult/widgets/app_bar/appbar_subtitle_two.dart';
 import 'package:educonsult/widgets/custom_text_form_field.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:educonsult/core/app_export.dart';
 import 'package:flutter/widgets.dart';
@@ -22,106 +23,301 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        
         resizeToAvoidBottomInset: true,
         appBar: _buildAppBar(context),
-        body: Container(
-          width: double.maxFinite,
-          padding: EdgeInsets.symmetric(horizontal: 15.h, vertical: 27.v),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildFiftyFive(context),
-              SizedBox(height: 9.v),
+        body: Column(
+          children: [
+            Expanded(
 
-              // Below is the code for CustomTextFormField which was given instead
-
-              // Padding(
-              //   padding: EdgeInsets.only(right: 2.h),
-              //   child: CustomTextFormField(
-              //     width: 158.h,
-              //     controller: imwaitingbroController,
-              //     hintText: "I'm waiting bro",
-              //     alignment: Alignment.centerRight,
-              //     contentPadding:
-              //         EdgeInsets.symmetric(horizontal: 19.h, vertical: 17.v),
-              //     borderDecoration:
-              //         TextFormFieldStyleHelper.fillPrimaryContainer,
-              //     filled: true,
-              //     fillColor: theme.colorScheme.primaryContainer,
-              //   ),
-              // ),
-              Align(
-                alignment: Alignment.centerRight,
+              child: SingleChildScrollView(
                 child: Container(
-                  // width: 200,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 23.h,
-                    vertical: 11.v,
-                  ),
-                  decoration: AppDecoration.fillBlue.copyWith(
-                      borderRadius: BorderRadiusStyle.roundedBorder14),
+                  height: MediaQuery.of(context).size.height,
+                  width: double.maxFinite,
+                  padding: EdgeInsets.symmetric(horizontal: 15.h, vertical: 27.v),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                        width: 185.h,
-                        margin: EdgeInsets.only(right: 12.h),
-                        child: const Text(
-                          "I'm waiting bro",
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 218, 225, 221),
+
+                      //_buildFiftyFive(context),
+                      SizedBox(height: 9.v),
+
+                      // Below is the code for CustomTextFormField which was given instead
+
+                      // Padding(
+                      //   padding: EdgeInsets.only(right: 2.h),
+                      //   child: CustomTextFormField(
+                      //     width: 158.h,
+                      //     controller: imwaitingbroController,
+                      //     hintText: "I'm waiting bro",
+                      //     alignment: Alignment.centerRight,
+                      //     contentPadding:
+                      //         EdgeInsets.symmetric(horizontal: 19.h, vertical: 17.v),
+                      //     borderDecoration:
+                      //         TextFormFieldStyleHelper.fillPrimaryContainer,
+                      //     filled: true,
+                      //     fillColor: theme.colorScheme.primaryContainer,
+                      //   ),
+                      // ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          // width: 200,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 23.h,
+                            vertical: 11.v,
+                          ),
+                          decoration: AppDecoration.fillBlue.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder14),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 185.h,
+                                margin: EdgeInsets.only(right: 12.h),
+                                child: const Text(
+                                  "I'm waiting bro",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 218, 225, 221),
+                                  ),
+                                ),
+                              )
+                            ],
                           ),
                         ),
-                      )
+                      ),
+
+                      SizedBox(height: 12.v),
+
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          // width: 200,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 23.h,
+                            vertical: 11.v,
+                          ),
+                          decoration: AppDecoration.fillOnPrimaryContainer.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder14),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 185.h,
+                                margin: EdgeInsets.only(right: 12.h),
+                                child: Text(
+                                    "Hi Frank do you watch NBA match last night?",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: theme.textTheme.bodyMedium),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 12.v),
+
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          // width: 200,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 23.h,
+                            vertical: 11.v,
+                          ),
+                          decoration: AppDecoration.fillBlue.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder14),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 185.h,
+                                margin: EdgeInsets.only(right: 12.h),
+                                child: const Text(
+                                  "I'm waiting bro",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 218, 225, 221),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 12.v),
+
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          // width: 200,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 23.h,
+                            vertical: 11.v,
+                          ),
+                          decoration: AppDecoration.fillOnPrimaryContainer.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder14),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 185.h,
+                                margin: EdgeInsets.only(right: 12.h),
+                                child: Text(
+                                    "Hi Frank do you watch NBA match last night?",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: theme.textTheme.bodyMedium),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 12.v),
+
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          // width: 200,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 23.h,
+                            vertical: 11.v,
+                          ),
+                          decoration: AppDecoration.fillBlue.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder14),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 185.h,
+                                margin: EdgeInsets.only(right: 12.h),
+                                child: const Text(
+                                  "I'm waiting bro",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 218, 225, 221),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 12.v),
+
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          // width: 200,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 23.h,
+                            vertical: 11.v,
+                          ),
+                          decoration: AppDecoration.fillOnPrimaryContainer.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder14),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 185.h,
+                                margin: EdgeInsets.only(right: 12.h),
+                                child: Text(
+                                    "Hi Frank do you watch NBA match last night?",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: theme.textTheme.bodyMedium),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 12.v),
+
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          // width: 200,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 23.h,
+                            vertical: 11.v,
+                          ),
+                          decoration: AppDecoration.fillBlue.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder14),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 185.h,
+                                margin: EdgeInsets.only(right: 12.h),
+                                child: const Text(
+                                  "I'm waiting bro",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 218, 225, 221),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 12.v),
+
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Container(
+                          // width: 200,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 23.h,
+                            vertical: 11.v,
+                          ),
+                          decoration: AppDecoration.fillOnPrimaryContainer.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder14),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 185.h,
+                                margin: EdgeInsets.only(right: 12.h),
+                                child: Text(
+                                    "Hi Frank do you watch NBA match last night?",
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: theme.textTheme.bodyMedium),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+
+
+
+
+
+
+
                     ],
                   ),
                 ),
               ),
-
-              SizedBox(height: 12.v),
-
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  // width: 200,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 23.h,
-                    vertical: 11.v,
+            ),
+            //const Spacer(),
+            //SizedBox(height: 16.v),
+            Padding(
+              padding: EdgeInsets.only(left: 10.h, right: 10.h,bottom: 10),
+              child: CustomTextFormField(
+                  //maxLines: null,
+                  controller: typehereController,
+                  hintText: "Type here...",
+                  hintStyle: const TextStyle(
+                    color: Color.fromARGB(255, 132, 131, 131),
                   ),
-                  decoration: AppDecoration.fillOnPrimaryContainer.copyWith(
-                      borderRadius: BorderRadiusStyle.roundedBorder14),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 185.h,
-                        margin: EdgeInsets.only(right: 12.h),
-                        child: Text(
-                            "Hi Frank do you watch NBA match last night?",
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: theme.textTheme.bodyMedium),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-
-              const Spacer(),
-              SizedBox(height: 16.v),
-              Padding(
-                padding: EdgeInsets.only(left: 5.h, right: 3.h),
-                child: CustomTextFormField(
-                    maxLines: null,
-                    controller: typehereController,
-                    hintText: "Type here...",
-                    hintStyle: const TextStyle(
-                      color: Color.fromARGB(255, 132, 131, 131),
-                    ),
-                    textInputAction: TextInputAction.newline),
-              )
-            ],
-          ),
+                  textInputAction: TextInputAction.newline),
+            )
+          ],
         ),
       ),
     );
@@ -170,23 +366,23 @@ class ChatScreen extends StatelessWidget {
   }
 
   /// Section Widget
-  Widget _buildFiftyFive(BuildContext context) {
-    return Align(
-        alignment: Alignment.center,
-        child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15.h),
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Padding(
-                  padding: EdgeInsets.only(top: 9.v, bottom: 10.v),
-                  child: SizedBox(width: 120.h, child: Divider())),
-              Padding(
-                  padding: EdgeInsets.only(left: 9.h),
-                  child: Text("Today", style: theme.textTheme.bodyMedium)),
-              Padding(
-                  padding: EdgeInsets.symmetric(vertical: 9.v),
-                  child: SizedBox(width: 122.h, child: Divider(indent: 6.h)))
-            ])));
-  }
+  // Widget _buildFiftyFive(BuildContext context) {
+  //   return Align(
+  //       alignment: Alignment.center,
+  //       child: Padding(
+  //           padding: EdgeInsets.symmetric(horizontal: 15.h),
+  //           child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+  //             Padding(
+  //                 padding: EdgeInsets.only(top: 9.v, bottom: 10.v),
+  //                 child: SizedBox(width: 120.h, child: Divider())),
+  //             Padding(
+  //                 padding: EdgeInsets.only(left: 9.h),
+  //                 child: Text("Today", style: theme.textTheme.bodyMedium)),
+  //             Padding(
+  //                 padding: EdgeInsets.symmetric(vertical: 9.v),
+  //                 child: SizedBox(width: 122.h, child: Divider(indent: 6.h)))
+  //           ])));
+  // }
 
   /// Navigates back to the previous screen.
   onTapArrowLeft(BuildContext context) {
